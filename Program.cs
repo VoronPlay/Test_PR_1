29525b835f19aa812ad8dffdb4c753bd3d49ebce
u31510 <u31510@P31510>
2022-11-19 08:49:51 +1000

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+        public class Program
+        {
+            public static void Main()
+            {
+                Console.WriteLine("Hello World!");
+            }
+
+            public static void SayGoodbye()
+            {
+                Console.WriteLine("Goodbye World!");
+            }
+        }
+}
+
